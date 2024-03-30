a23c34eb654e5b0e12398b4c749d5775f2e7b91c
Kevin Arias <aa-kevin@hotmail.com>
2024-03-30 08:01:40 -0600

    Update favicon and logo image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,14 @@ import { Editor } from '../components';
 export const Home = () => {
   return (
     <div
-      className="flex min-h-screen w-screen items-center justify-center bg-white bg-bottom bg-no-repeat bg-cover"
+      className="flex min-h-screen w-screen items-center justify-center bg-white bg-cover bg-bottom bg-no-repeat"
       style={{
         backgroundImage: 'url(/images/waves.svg)',
       }}
     >
       <main className="min-h-screen w-full md:max-w-7xl">
-        <header className="p-10 text-center">
-          <img src="/images/NoteCodeLogo.svg" alt="Note Code Logo" className="mb-8 inline-block" />
+        <header className="p-5 text-center">
+        <img src="/images/logo.webp" alt="Note Code Logo" className="inline-block w-24" />
           <h3 className="text-[2rem]">Create & Share</h3>
           <h1 className="text-[2.5rem]">Your Code easily</h1>
         </header>
